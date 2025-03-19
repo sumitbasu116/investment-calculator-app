d5a2ca1549993efc939a7be9e89f632308d488a3
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-19 23:56:59 +0530

    user input form submit

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
 @Component({
@@ -8,5 +8,8 @@ import { FormsModule } from "@angular/forms";
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  onSubmit() {
+    console.log("SUBMITTED!!");
+  }
 
 }
