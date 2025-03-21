649b8343f7ac9b4b3352baba9d3677894f54bd93
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-21 23:31:25 +0530

    introducing currency pipe

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { UserInput } from '../user-input/userInput.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
